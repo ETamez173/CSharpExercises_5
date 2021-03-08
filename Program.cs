8c1d177d0ff1f82fd068e4c12fab127ff49729ed
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-08 16:13:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Data Types | C# | Tutorial 5 
+
+namespace CSharpExercises_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Academy";
+            char grade = 'A';
+            int age = 30;
+            // decimal numbers - float, double, decimal - 
+            // less to more precise
+            double gpa = 3.2;
+            bool isMale = true;
+
+            Console.WriteLine(grade);
+            Console.WriteLine(isMale);
+
+            Console.ReadLine();
+        }
+    }
+}
